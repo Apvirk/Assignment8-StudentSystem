4784c70c7b2692ffb0c1c5996c4fbc14645fce81
Ap Virk <avirk811@gmail.com>
2019-02-26 22:38:23 +0000

    Add project files.

---
+++ StudentSystem/Controllers/ClassDetailsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentSystem;
+
+namespace StudentSystem.Controllers
+{
+    public class ClassDetailsController : Controller
+    {
+        private StudentInfoEntities db = new StudentInfoEntities();
+
+        // GET: ClassDetails
+        public ActionResult Index()
+        {
+            var classDetails = db.ClassDetails.Include(c => c.Branch);
+            return View(classDetails.ToList());
+        }
+
+        // GET: ClassDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassDetail classDetail = db.ClassDetails.Find(id);
+            if (classDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classDetail);
+        }
+
+        // GET: ClassDetails/Create
+        public ActionResult Create()
+        {
+            ViewBag.BranchID = new SelectList(db.Branches, "ID", "Trade");
+            return View();
+        }
+
+        // POST: ClassDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,BranchID,ClassName")] ClassDetail classDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ClassDetails.Add(classDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BranchID = new SelectList(db.Branches, "ID", "Trade", classDetail.BranchID);
+            return View(classDetail);
+        }
+
+        // GET: ClassDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassDetail classDetail = db.ClassDetails.Find(id);
+            if (classDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BranchID = new SelectList(db.Branches, "ID", "Trade", classDetail.BranchID);
+            return View(classDetail);
+        }
+
+        // POST: ClassDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,BranchID,ClassName")] ClassDetail classDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(classDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BranchID = new SelectList(db.Branches, "ID", "Trade", classDetail.BranchID);
+            return View(classDetail);
+        }
+
+        // GET: ClassDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClassDetail classDetail = db.ClassDetails.Find(id);
+            if (classDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classDetail);
+        }
+
+        // POST: ClassDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ClassDetail classDetail = db.ClassDetails.Find(id);
+            db.ClassDetails.Remove(classDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StudentSystem/Controllers/MarkdetailsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentSystem;
+
+namespace StudentSystem.Controllers
+{
+    public class MarkdetailsController : Controller
+    {
+        private StudentInfoEntities db = new StudentInfoEntities();
+
+        // GET: Markdetails
+        public ActionResult Index()
+        {
+            var markdetails = db.Markdetails.Include(m => m.ClassDetail).Include(m => m.Student);
+            return View(markdetails.ToList());
+        }
+
+        // GET: Markdetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Markdetail markdetail = db.Markdetails.Find(id);
+            if (markdetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(markdetail);
+        }
+
+        // GET: Markdetails/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClassID = new SelectList(db.ClassDetails, "ID", "ClassName");
+            ViewBag.Roll = new SelectList(db.Students, "ID", "FirstName");
+            return View();
+        }
+
+        // POST: Markdetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Roll,ClassID,ObtainMark,OutOf,Percentage,Pass")] Markdetail markdetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Markdetails.Add(markdetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClassID = new SelectList(db.ClassDetails, "ID", "ClassName", markdetail.ClassID);
+            ViewBag.Roll = new SelectList(db.Students, "ID", "FirstName", markdetail.Roll);
+            return View(markdetail);
+        }
+
+        // GET: Markdetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Markdetail markdetail = db.Markdetails.Find(id);
+            if (markdetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClassID = new SelectList(db.ClassDetails, "ID", "ClassName", markdetail.ClassID);
+            ViewBag.Roll = new SelectList(db.Students, "ID", "FirstName", markdetail.Roll);
+            return View(markdetail);
+        }
+
+        // POST: Markdetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Roll,ClassID,ObtainMark,OutOf,Percentage,Pass")] Markdetail markdetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(markdetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClassID = new SelectList(db.ClassDetails, "ID", "ClassName", markdetail.ClassID);
+            ViewBag.Roll = new SelectList(db.Students, "ID", "FirstName", markdetail.Roll);
+            return View(markdetail);
+        }
+
+        // GET: Markdetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Markdetail markdetail = db.Markdetails.Find(id);
+            if (markdetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(markdetail);
+        }
+
+        // POST: Markdetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Markdetail markdetail = db.Markdetails.Find(id);
+            db.Markdetails.Remove(markdetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
